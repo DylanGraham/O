63dbd544517eaf6f84b7e1472b0182d2c4107d25
Dylan Graham <dylan.graham@gmail.com>
2017-03-24 13:58:15 +1100

    justify

--- app/src/main/java/com/androidnoob/o/MainActivity.java
+++ app/src/main/java/com/androidnoob/o/MainActivity.java
@@ -13,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.tv1);
+        TextView textView = (TextView) findViewById(R.id.tv1);
 
         textView.setText(LocalDate.now().toString());
+
+        textView.setJustify(true);
     }
 }
